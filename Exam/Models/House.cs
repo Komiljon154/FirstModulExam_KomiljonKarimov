1e704ec7bb496cb62be88dbc0010c17b2f43732d
Komiljon154 <komiljonk154@gmail.com>
2024-11-30 11:33:19 +0500

    code addet and finished

---
+++ Exam/Models/House.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamFirsModul.Models
+{
+    internal class House
+    {
+        public Guid Id { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+        public Double Price { get; set; }
+        public int QuentityRooms { get; set; }
+
+    }
+}
---
+++ Exam/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+
+namespace Exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        ///1-misol
+        public static string ReturnLastFourWord(string word)
+        {
+            word = word.Substring((word.Length - 1) - 3);
+            return word;
+        }
+        ///2-misol
+        public static int OddCounter(List<int> number)
+        {
+            var counter = 0;
+            foreach (var item in number)
+            {
+                while (item / 2 != 0)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+
+
+        }
+        ///3-misol
+        public static bool EndWith_g_10(string text)
+        {
+            if (text.EndsWith("g_10"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        ///4-misol
+        public static bool LengthGreaterThanThree(List<string> text)
+        {
+            var check = false;
+            foreach (string lists in text)
+            {
+                if (text.Count > 3)
+                {
+                    check = true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return check;
+
+        }
+
+    }
+}
